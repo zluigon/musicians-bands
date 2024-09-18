8cbf8b24addf92518630bcb4d5c4d1e26f637898
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-18 10:29:58 -0400

    Update: db connection established

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
-const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const path = require("path");
+const { Sequelize, DataTypes } = require("sequelize");
 
 // TODO - create the new sequelize connection
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+});
 
 module.exports = {
-    sequelize,
-    Sequelize
+  db,
+  DataTypes,
+  Sequelize,
 };
