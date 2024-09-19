ef2bd9ee56868afd03588bdd18bb599aa7cfdcee
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-19 14:22:35 -0400

    Update: Many-to-Many association created w/ passing tests

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { Song } = require("./models/Song");
 Band.hasMany(Musician);
 Musician.belongsTo(Band);
 
+Band.belongsToMany(Song, { through: "band_songs" });
+Song.belongsToMany(Band, { through: "band_songs" });
+
 module.exports = {
   Band,
   Musician,
